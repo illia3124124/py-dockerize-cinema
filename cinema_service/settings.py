a4ac40a9d477e63ec916feb9711887926fda3133
illa-j <iliacdmn@gmail.com>
2026-01-19 18:28:28 +0200

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -33,10 +33,13 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost"
+]
 
 INTERNAL_IPS = [
-    "127.0.0.1",
+    "127.0.0.1"
 ]
 
 # Application definition
